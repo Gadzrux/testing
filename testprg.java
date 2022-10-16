53e70c891b76bfb7b5059406597ee6a35e35dae3
Krishnava Ghosh <krishnavaghosh23@gmail.com>
2022-10-16 20:17:11 +0530

    new files
    .

---
+++ testprg.java
@@ -0,0 +1,11 @@
+class testprg
+{
+    public static void main (String args[])
+    {
+        int a,b,sum;
+        a = 1;
+        b = 2;
+        sum = a + b;
+        System.out.println("Sum is : " + sum);
+    }
+}
